b77bb375c74dba3be9527c8d235f24984578b97d
vaibhav sharma <vaibhavsharma@vaibhavs-MBP.lan>
2017-11-28 22:38:59 +0530

    changes to incorporate bootstrap

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      // Add your component markup and other subcomponent references here.
+      <div className='application'>
+        
+        <h1>Hello, World!</h1>
+      </div>  
+    );
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,11 @@ module.exports = {
         test: /\.css$/,
         use: [ 'style-loader', 'css-loader' ]
         },
+        { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader" },
+        { test: /\.(woff|woff2)$/, loader:"url-loader?prefix=font/&limit=5000" },
+        { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=application/octet-stream" },
+        { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=image/svg+xml" },
+        { test: /\.json$/, loader: 'json-loader'},
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
     ]
