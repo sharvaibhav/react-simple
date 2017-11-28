902e6cb365490bf339a3e4285652a118f9649bf9
vaibhav sharma <vaibhavsharma@vaibhavs-MBP.lan>
2017-11-28 22:55:37 +0530

    changes for simple application plane

--- src/app.js
+++ src/app.js
@@ -4,9 +4,14 @@ export default class App extends Component {
   render() {
     return (
       // Add your component markup and other subcomponent references here.
-      <div className='application'>
-        
-        <h1>Hello, World!</h1>
+      <div className='application container-fluid'>
+        <div className='row header'>
+            <div className="col-md-4">Left section of Header</div>
+            <div className="col-md-4 col-md-offset-4">Right Section of the Header</div>
+        </div>
+        <div className='row middle-section'>
+            <h2> The navigation section </h2>
+        </div>
       </div>  
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
 import '../node_modules/bootstrap3/dist/css/bootstrap.css';
+import '../src/css/charts.css';
 
 render(
   <App />,
